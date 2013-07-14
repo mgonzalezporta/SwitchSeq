31d5669dc7cc258aa5223f701590c2fd11c12858
Mar Gonzàlez-Porta <mar@ebi.ac.uk>
2013-07-14 22:15:33 +0100

    Segment plots implemented

--- scripts/_lib.R
+++ scripts/_lib.R
@@ -20,9 +20,10 @@ new_switch_event=function(gId, input_file, ensembl_annot) {
 
 	# format data
 	data=merge(tBiotype, data, by="tId")
-	header=paste(data$tId, data$tBiotype, sep=" - ")
+	header=paste(data$tBiotype, data$tId, sep=" - ")
 	data=t(data[,-c(1:2)])
 	colnames(data)=header
+	data=data[,order(colnames(data))]
 
 	return(data)
 }
@@ -48,6 +49,15 @@ get_outfile=function(out_dir, plot, gId) {
 	outfile=paste(out_subdir, "/", gId, ".pdf", sep="")
 
 	return(outfile)
+}
+
+summarise_gexp=function (gexp, cond, text) {
+	x=gexp[cond]
+	x_m=formatC(round(mean(x), 2), 2, format="f")
+	x_sd=formatC(round(sd(x), 2), 2, format="f")
+
+	result=paste(text, " - mean=", x_m, ", sd=", x_sd, sep="")
+	return(result)
 }
 
 plot_stars=function(gId, x, gexp, cond1, cond2, outfile) {
@@ -156,21 +166,129 @@ plot_distrplot=function(gId, x, gexp, cond1, cond2, outfile) {
 }
 
 plot_segments=function(gId, data, gexp, cond1, cond2, outfile) {
+
 	nOfT=dim(data)[2]
-	# gexp
-	x=c(0.1,0.2)
-	y=c(mean(gexp[cond1]), mean(gexp[cond2]))
-	sd=c(sd(gexp[cond1]), sd(gexp[cond2]))
-	m=cbind(x,y,sd)
+	plot_count=0
+
+	col=c(
+		rep(list(c("gray60", "gray30")), 2)
+		)
+	col=c(col, get_transcript_colors(data))
+
+	mar=c(
+		list(c(0, 4, 0, 3)), 
+		list(c(0, 2, 0, 5)), 
+		rep(list(c(0, 0, 0, 0)), nOfT))
 
 	# plot
-	pdf(file=outfile, height=6, width=(4+nOfT-1))
-	#layout(1:(nOfT+2), widths=rep(2, nOfT+2))
-	layout(t(1:3), widths=c(1,1,6))
+	pdf(file=outfile, width=(4+nOfT-1), height=6)
+		layout(t(1:(nOfT+2)), widths=c(rep(1, 2), rep(0.5, nOfT)))
+		par(oma=c(13,0,2,0))
 
-		# gene expression
-		par(bty="n")
+		# gexp
+		data_cond1=gexp[cond1]
+		data_cond2=gexp[cond2]
+		xlab=gId
+		ylab="gene expression (RPKMs)"
+		type="gexp"
+		plot_count=plot_count+1
+		subplot_segments(data_cond1, data_cond2, xlab, ylab, type, mar, col, plot_count)
 
+		# dominance
+		data_cond1=calculate_dominance(data[cond1,])
+		data_cond2=calculate_dominance(data[cond2,])
+		xlab=gId
+		ylab="major transcript dominance"
+		type="dom"
+		plot_count=plot_count+1
+		subplot_segments(data_cond1, data_cond2, xlab, ylab, type, mar, col, plot_count)
+		
+		# transcript relative abundances
+		for(i in 1:nOfT){
+			data_cond1=data[cond1,i]
+			data_cond2=data[cond2,i]
+			xlab=gsub(" - ", "\n", colnames(data)[i])
+			ylab="transcript relative abundance"
+			type="trel"
+			plot_count=plot_count+1
+			subplot_segments(data_cond1, data_cond2, xlab, ylab, type, mar, col, plot_count)
+		}
+	garbage<-dev.off()	# supress output
+}
+
+get_transcript_colors=function(x) {
+
+	nOfT=dim(x)[2]
+
+	biotypes=unlist(strsplit(colnames(x), " - "))
+	biotypes=data.frame(table(biotypes[seq(1, length(biotypes), by=2)]))
+	colnames(biotypes)=c("tBiotype", "n")
+	
+	col_intervals=data.frame(t(matrix(c(0.05, 0.15, 0.3, 0.4, 0.6, 0.7, 0.85, 0.95), nrow=2)))
+	colnames(col_intervals)=c("start", "end")
+	rownames(col_intervals)=NULL
+	col_intervals$tBiotype=c("nonsense_mediated_decay", "processed_transcript",
+		"protein_coding", "retained_intron")
+	
+	m=merge(biotypes, col_intervals, by="tBiotype")
+	
+	#col=t(data.frame(NA, NA))
+	#rownames(col)=c("N", "T")
+	result=list()
+	for (i in 1:dim(m)[1]) {
+		biotype=as.character(m[i,]$tBiotype)
+        n=m[i,]$n
+        start=m[i,]$start
+        end=m[i,]$end
+
+        col=c(
+			rainbow( n, s = 0.7, v = 0.9, alpha=0.7, start=start, end=end),
+        	rainbow( n, s = 0.7, v = 0.5, alpha=0.7, start=start, end=end)
+        )
+        names(col)=rep(paste(biotype, 1:n, sep="_"), 2)
+        result=c(result, split(col, names(col)))
+
+  #       result=c(result, list())
+
+		# tcol=t(data.frame(
+		# ))
+		# rownames(tcol)=c("N", "T")
+		# col=cbind(col, tcol)
+	}
+	return(result)
+
+}
+
+get_ratio_second=function(x) {
+	x=x[order(x, decreasing=T)]
+	r=x[2]/x[1]
+	return(r)
+}
+
+calculate_dominance=function(x) {
+	r=apply(x, 1, get_ratio_second)
+	return(r)
+}
+
+subplot_segments=function(data_cond1, data_cond2, xlab, ylab, type, mar, col, plot_count) {
+
+	par(bty="n", mar=mar[[plot_count]])	
+
+	# data points
+	x=c(0.1,0.2)
+	y=c(mean(data_cond1), mean(data_cond2))
+	sd=c(sd(data_cond1), sd(data_cond2))
+	m=cbind(x,y,sd)
+
+	# plot parameters
+	col=col[[plot_count]]
+	lwd=2
+	epsilon = 0.02
+	xlim=c(0,0.3)
+
+	# set ylim
+	ylim=c(0,1)
+	if (type == "gexp") {
 		min_y=min(m[, "y"])
 		max_y=max(m[, "y"])
 		row_index_min=which(m[, "y"]==min_y)
@@ -180,33 +298,22 @@ plot_segments=function(gId, data, gexp, cond1, cond2, outfile) {
 			row_index_max=1
 		}
 		ylim=c( min_y-m[row_index_min, "sd"]-1, max_y+m[row_index_max, "sd"]+1 )
-
-		col=c("red", "blue")
-		#plot(x,y,xlim=c(0,0.3),ylim=ylim, xlab="", yaxt="n", col=c("white", "gray90"))
-		plot(x,y,xlim=c(0,0.3), ylim=ylim, col=col, xaxt="n", ylab="")
-		segments(x, y-sd,x, y+sd, col=col)
-		epsilon = 0.02
-		segments(x-epsilon,y-sd,x+epsilon,y-sd, col=col)
-		segments(x-epsilon,y+sd,x+epsilon,y+sd, col=col)
-
+	}
 	
-	 #mtext("gene expression (FPKMs)", 2.5, 1)
-	 # axis(1, labels=F, lwd.ticks=0, lwd=1, at=c(1,2))
-	 # mtext(gId, 0.75, 2)
-	
-	# frame()
-		# par
-	#	par(oma=c(4.5, 12, 0.5, 0.5), las=1, cex=1)
-	garbage<-dev.off()	# supress output
-}
+	# plot
+	plot(x, y, xlim=xlim, ylim=ylim, col=col, xaxt="n", yaxt="n", ylab="", xlab="", lwd=lwd)
+	segments(x, y-sd, x, y+sd, col=col, lwd=lwd)
+	segments(x-epsilon,y-sd,x+epsilon,y-sd, col=col, lwd=lwd)
+	segments(x-epsilon,y+sd,x+epsilon,y+sd, col=col, lwd=lwd)
 
-summarise_gexp=function (gexp, cond, text) {
-	x=gexp[cond]
-	x_m=formatC(round(mean(x), 2), 2, format="f")
-	x_sd=formatC(round(sd(x), 2), 2, format="f")
+	# axes and axes labels
+	if (plot_count %in% 1:3){
+        	axis(2, las=0, pos=0)
+	        mtext(ylab, side=2, line=2.5, cex=0.75)
+    }
+	axis(1, labels=F, lwd.ticks=0, lwd=1, at=c(0.1,0.2))
+	mtext(xlab, side=1, line=0.75, las=2, cex=0.75)
 
-	result=paste(text, " - mean=", x_m, ", sd=", x_sd, sep="")
-	return(result)
 }
 
 plot_boxplots=function(gId, x, gexp) {
