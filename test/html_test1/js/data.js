414fb0abb09a0ed01c6669c854ebaa14c8298af0
Mar Gonzàlez-Porta <mar@ebi.ac.uk>
2013-07-26 11:43:37 +0100

    test output added

---
+++ test/html_test1/js/data.js
@@ -0,0 +1,43 @@
+var data = 
+[
+   {
+      "C2_tExp" : "3",
+      "C1_biotype" : "protein_coding",
+      "C1_tId" : "ENST00000373616",
+      "pIdentity" : "95.6",
+      "C1_breadth" : "100.00",
+      "nOfT" : "11",
+      "C2_gExp" : "4",
+      "C2_biotype" : "protein_coding",
+      "C1_principal" : "YES",
+      "gId" : "ENSG00000137409",
+      "C2_breadth" : "75.00",
+      "C1_tExp" : "4",
+      "C1_gExp" : "4",
+      "C2_principal" : "NO",
+      "pdbEntry" : "NO",
+      "C2_tId" : "ENST00000373627",
+      "gName" : "MTCH1",
+      "rank" : "13.25"
+   },
+   {
+      "C2_tExp" : "3",
+      "C1_biotype" : "protein_coding",
+      "C1_tId" : "ENST00000244020",
+      "pIdentity" : "NA",
+      "C1_breadth" : "100.00",
+      "nOfT" : "2",
+      "C2_gExp" : "4",
+      "C2_biotype" : "nonsense_mediated_decay",
+      "C1_principal" : "YES",
+      "gId" : "ENSG00000124193",
+      "C2_breadth" : "75.00",
+      "C1_tExp" : "4",
+      "C1_gExp" : "4",
+      "C2_principal" : "NO",
+      "pdbEntry" : "NO",
+      "C2_tId" : "ENST00000483871",
+      "gName" : "SRSF6",
+      "rank" : "13.25"
+   }
+]
