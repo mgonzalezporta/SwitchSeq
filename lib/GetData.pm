c5348cae6ca131436d48a72364c4dc3361acce6b
Mar Gonzàlez-Porta <mar@ebi.ac.uk>
2014-01-14 22:15:58 +0000

    Changed GetData.pm

--- lib/GetData.pm
+++ lib/GetData.pm
@@ -12,33 +12,37 @@ our @EXPORT = qw( get_data );
 sub get_data {	
 	## collect arguments
 	my $ref_arguments=$_[0];
-	my %arguments=%{$ref_arguments};
-	my $data_dir=$arguments{'data_dir'};
-	my $species=$arguments{'species'};
-	my $ensembl_v=$arguments{'ensembl_v'};
+	my $data_dir=$ref_arguments->{'data_dir'};
+	my $species=$ref_arguments->{'species'};
+	my $ensembl_v=$ref_arguments->{'ensembl_v'};
+
+	## expand species name
+	my %species_full=(
+		hsa	    => "homo_sapiens",
+		mmu	    => "mus_musculus",
+		rno	    => "rattus_norvegicus",
+		dre	    => "danio_rerio"
+	);
 
 	## define urls
+	## by default, work with the latest version of APPRIS
 	my %url=(
-		ensembl_1 	=> "http://www.ebi.ac.uk/~mar/tools/lorem/$species/_ensembl$ensembl_v.annot_coding.1.txt",
-		ensembl_2	=> "http://www.ebi.ac.uk/~mar/tools/lorem/$species/_ensembl$ensembl_v.annot_coding.2.txt",
-		prot_seq    => "http://www.ebi.ac.uk/~mar/tools/lorem/$species/_ensembl$ensembl_v.prot_seq.tar.gz",
-		appris      => "http://www.ebi.ac.uk/~mar/tools/lorem/$species/_appris.results.rel15.9Jun2013.v2.main.tsv",
-		css         => "http://www.ebi.ac.uk/~mar/tools/lorem/css.tar.gz",
-		js          => "http://www.ebi.ac.uk/~mar/tools/lorem/js.tar.gz"
+		ensembl_1   => "http://www.ebi.ac.uk/~mar/tools/lorem/$species.$ensembl_v/annot_coding.1.txt",
+		ensembl_2   => "http://www.ebi.ac.uk/~mar/tools/lorem/$species.$ensembl_v/annot_coding.2.txt",
+		prot_seq    => "http://www.ebi.ac.uk/~mar/tools/lorem/$species.$ensembl_v/prot_seq.tar.gz",
+		appris      => "http://appris.bioinfo.cnio.es/download/data/$species_full{$species}/appris_data.principal.txt",
 	);
 
 	## create directory structure
 	unless ( -e "$data_dir" ) { system("mkdir $data_dir") };
 	unless ( -e "$data_dir/$species" ) { system("mkdir $data_dir/$species") };
 	
-	## download files
+	## obtain files
 	print "# Obtaining the necessary files...\n";
 	_get_file($url{'ensembl_1'}, "$data_dir/$species");
 	_get_file($url{'ensembl_2'}, "$data_dir/$species");
 	_get_file($url{'prot_seq'}, "$data_dir/$species");
 	_get_file($url{'appris'}, "$data_dir/$species");
-	_get_file($url{'css'}, $data_dir);
-	_get_file($url{'js'}, $data_dir);
 	print "# Data files saved under $data_dir\n";
 }
 
@@ -51,8 +55,8 @@ sub _get_file {
 	if ($file =~ /.tar.gz$/) {
 		my $fname=( split(/\//, $file) )[-1];
 		system("cd $dir;
-				tar xzf $fname;
-				rm $fname");
+			tar xzf $fname;
+			rm $fname");
 	}
 }
 
