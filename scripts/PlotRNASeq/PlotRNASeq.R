74ebe489fc6bd2eadb874226849cc8a4a739df8d
Mar Gonzàlez-Porta <mar@ebi.ac.uk>
2014-01-17 18:05:56 +0000

    adapted Lorem to work with Ipsum

--- scripts/PlotRNASeq/PlotRNASeq.R
+++
@@ -1,4 +0,0 @@
-source("AllClasses.R")
-source("AllGenerics.R")
-source("methods-TranscriptExpressionSet.R")
-source("functions.R")
--- scripts/generate_plots.R
+++ scripts/generate_plots.R
@@ -3,7 +3,29 @@ for(i in 1:length(args)){
     eval(parse(text=args[[i]]))
 }
 
-source(paste(bin, "/scripts/PlotRNASeq/PlotRNASeq.R", sep=""), chdir=T)
+library("Ipsum")
+
+## functions
+getOutfile=function(outdir=outdir, plot_type=plot_type, gId=gId) {
+  # create plots directory
+  outdir_plots=paste(outdir, "/data/plots", sep="")
+  dir.create(outdir_plots, showWarnings = FALSE)
+
+  # create plot type directory
+  outdir_plots_type=paste(outdir_plots, plot_type, sep="/")
+  dir.create(outdir_plots_type, showWarnings = FALSE)
+
+  # create gId subdirectory
+  s=unlist(strsplit(gId, ''))
+  id=paste(s[c(1:(length(s)-3))], collapse='')
+  outdir_plots_type_id=paste(outdir_plots_type, id, sep="/")
+  dir.create(outdir_plots_type_id, showWarnings = FALSE)
+
+  # outfile
+  outfile=paste(outdir_plots_type_id, "/", gId, ".pdf", sep="")
+
+  return(outfile)
+}
 
 ## load data
 rpkms=readExpressionData(gId=gId, infile=expdata, cond1=cond1, cond2=cond2)
