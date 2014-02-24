8de7c4bbd7b9b3b53c235fca89882f896f0bf580
Mar Gonzàlez-Porta <mar@ebi.ac.uk>
2014-02-24 15:18:17 +0000

    matched execute.sh with example from wiki

--- resources/get_data/EXECUTE.sh
+++ resources/get_data/EXECUTE.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 
-# MMU.74
-species=mmu
-ensembl_v=74
-outdir=/homes/mar/public_html/tools/switchseq/data_for_download
-path=http://www.ensembl.org/biomart/martservice?
-c="./get_data.sh $species $ensembl_v $outdir $path"
-bsub $c
-
-# HSA.74
+# HSA.70
 species=hsa
-ensembl_v=74
-outdir=/homes/mar/public_html/tools/switchseq/data_for_download
-path=http://www.ensembl.org/biomart/martservice?
-c="./get_data.sh $species $ensembl_v $outdir $path"
-bsub $c
-
-# RNO.70
-species=rno
-ensembl_v=70
-outdir=/homes/mar/public_html/tools/switchseq/data_for_download
-path=http://jan2013.archive.ensembl.org/biomart/martservice?
-c="./get_data.sh $species $ensembl_v $outdir $path"
-bsub $c
-
-# DRE.70
-species=dre
 ensembl_v=70
 outdir=/homes/mar/public_html/tools/switchseq/data_for_download
 path=http://jan2013.archive.ensembl.org/biomart/martservice?
